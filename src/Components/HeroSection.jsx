954db57a1deb970d3b2b1e4e8bf1ce1374b455bf
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-08-03 14:59:34 +0430

    add atropos

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,28 +1,45 @@
 import { FaPlay } from "react-icons/fa";
 import banner from "../assets/banner.jpg";
+import Atropos from "atropos/react";
+import "atropos/css"; // Don't forget the CSS import
+
 export default function HeroSection() {
   return (
     <section className="w-full container max-sm:px-[1px]">
-      <div className="relative max-sm:rounded-none rounded-3xl shadow-2xl shadow-primary/60 overflow-hidden h-[550px]">
-        <img src={banner} alt="banner" className="absolute w-full h-full object-cover" />
-        <div className="absolute flex justify-center items-center bg-secondary/75 inset-0">
-          <div className="flex flex-col items-center space-y-6 sm:space-y-6 text-white mt-10">
-            <h2 className="px-4 sm:px-0 text-4xl md:text-5xl font-extrabold drop-shadow-[0_0_4px_rgba(0,0,0,0.5)]">
-              دوره ورکشـاپ ویدئویـی درآمـد دلاری
-            </h2>
-            <p className="sm:text-lg mx-auto w-full px-4 sm:px-0 sm:w-2/3 leading-10 opacity-70 drop-shadow-[0_0_4px_rgba(0,0,0,0.2)]">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم چاپ و با استفاده از
-              طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
-              سطرآنچنان که لازم است...
-            </p>
-            <div className="p-2 bg-background/40 hover:bg-transparent transition-all cursor-pointer rounded-full">
-              <button className="p-4  bg-white cursor-pointer text-yellow-400 rounded-full ">
-                <FaPlay size={20} />
-              </button>
+      <Atropos
+        className="w-full h-full bg-background inset-0 max-sm:rounded-none rounded-3xl"
+        shadow={false}
+        rotateXMax={2}
+        rotateYMax={2}
+        activeOffset={10}
+        duration={200}
+      >
+        <div className="relative max-sm:rounded-none rounded-3xl overflow-hidden h-[550px] ">
+          <img
+            src={banner}
+            alt="banner"
+            className="w-full h-full p-1 max-sm:rounded-none rounded-3xl object-cover "
+          />
+
+          <div className="absolute flex justify-center items-center bg-secondary/75 inset-0">
+            <div className="flex flex-col items-center space-y-6 sm:space-y-6 text-white mt-10">
+              <h2 className="px-4 sm:px-0 text-4xl md:text-5xl font-extrabold drop-shadow-[0_0_4px_rgba(0,0,0,0.5)]">
+                دوره ورکشـاپ ویدئویـی درآمـد دلاری
+              </h2>
+              <p className="sm:text-lg mx-auto w-full px-4 sm:px-0 sm:w-2/3 leading-10 opacity-70 drop-shadow-[0_0_4px_rgba(0,0,0,0.2)]">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم چاپ و با استفاده
+                از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون
+                و سطرآنچنان که لازم است...
+              </p>
+              <div className="p-2 bg-background/40 hover:bg-transparent transition-all cursor-pointer rounded-full">
+                <button className="p-4 bg-white cursor-pointer text-yellow-400 rounded-full">
+                  <FaPlay size={20} />
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Atropos>
     </section>
   );
 }
