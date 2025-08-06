98dac0a1d060b51eda1480d1682663f4d05b76d5
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-08-06 20:46:30 +0430

    Update NotFound.jsx

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
-    <div className="text-5xl flex flex-col mt-5">
-      404! <span className="text-xl mt-1">page not found</span>
+    <div className="min-h-screen text-5xl bg-background font-bold flex flex-col pt-5">
+      404! <span className="text-xl mt-1 font-medium">page not found</span>
     </div>
   );
 }
